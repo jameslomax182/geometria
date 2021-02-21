6016ea3d8a5bb30697e063619ef927d51d84142f
jameslomax182 <jameslomax182@hotmail.com>
2021-02-21 14:57:23 +0100

    Docuentar es de sabios, aunque leerla sea pesado
    generar javadoc en la carpeta ladocu

---
+++ ladocu/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Circulo_JRL","l":"area()"},{"p":"<Unnamed>","c":"Circulo_JRL","l":"Circulo_JRL(double, String)","u":"%3Cinit%3E(double,java.lang.String)"},{"p":"<Unnamed>","c":"Circulo_JRL","l":"perimetro()"}];updateSearchResults();
