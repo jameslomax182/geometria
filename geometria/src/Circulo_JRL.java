1e3aec549b8812d37b3318c215db102ae0a26f73
jameslomax182 <jameslomax182@hotmail.com>
2021-02-21 13:42:58 +0100

    version refactorizada
    refactorizado

--- geometria/src/Circulo_JRL.java
+++ geometria/src/Circulo_JRL.java
@@ -1,10 +1,10 @@
 
-public class Circulo extends FiguraGeometrica {
+public class Circulo_JRL extends FiguraGeometrica_JRL {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_JRL(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
